f05ef5a6d4247ef74f7916ea545bf38129cebe82
Saulo Costa <saulotarsobc@gmail.com>
2022-03-30 15:51:43 -0300

    :fire:

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const { Client } = require('whatsapp-web.js');
+const {
+    Client
+} = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal')
+const express = require('express')
+const app = express()
+const port = 3000
 
 const client = new Client();
 
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
     console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, {small:true})
+    qrcode.generate(qr, {
+        small: true
+    })
 });
 
 client.on('ready', () => {
@@ -20,3 +27,12 @@ client.on('message', msg => {
 });
 
 client.initialize();
+
+// express
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
